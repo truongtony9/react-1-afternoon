cb6b69ab74c5b7d2ca431dcf14f9524256fef1e7
truongtony9 <truong99t@gmail.com>
2018-06-03 23:20:47 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./index.css";
+import TopicBroswer from "./components/TopicBrowser/TopicBrowser";
 
 class App extends Component {
   render() {
-    
+    return <TopicBroswer />;
   }
 }
 
